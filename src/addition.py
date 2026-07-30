9e7fd31d43e2ab982566b1a06418901d146fcd58
sabita245 <ranisabita631@gmail.com>
2026-07-30 14:40:57 +0530

    fixing the linting error

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,12 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
+
+
 def test_add():
-    assert add(2,3) == 5
-    assert add(-1,1) == 0
-    assert add(0,0) == 0
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+
 if __name__ == "__main__":
     test_add()
